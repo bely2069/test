f0600d8a907e7e091da441d9070868ac440a348f
bely2069 <98699521+bely2069@users.noreply.github.com>
2022-01-30 18:19:15 +0200

    Update json1.py

--- json1.py
+++ json1.py
@@ -1,15 +1,19 @@
 import json
-
+import time
 file_name='not_valid_json.json'#path file
 try:
     with open(file_name, 'r') as f:
         try:
             data = json.load(f)#load file
             print('Proper file ')
+            time.sleep(5)
         except Exception as e:
             print('is not proper file '+e)
+            time.sleep(5)
 except :
     print('not found file')
+    time.sleep(5)
+
 
 
 
